bb5ddb66a9eef3bc83df10c0bfc15cd78df70897
Hector Ocampo <hector2603@users.noreply.github.com>
2017-10-12 11:01:43 -0500

    Inicio

---
+++ backpropagation/Prueba.py
@@ -0,0 +1,17 @@
+'''
+Created on 12/10/2017
+
+@author: hector
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
