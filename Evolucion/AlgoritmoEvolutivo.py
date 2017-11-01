b3c72d561c62bff377d2789f89409003ecf63355
Hector Ocampo <hector2603@users.noreply.github.com>
2017-11-01 00:02:23 -0500

    algunas pruebas con evvolutivos

--- Evolucion/AlgoritmoEvolutivo.py
+++ Evolucion/AlgoritmoEvolutivo.py
@@ -27,58 +27,112 @@ class Algoritmoevolutivo(object):
         
     def evolucion(self):
         #generar la poblacion inicial random
-        fMin = 0.006
         for x in range(self.tamanoPoblacion):
-            self.poblacion.append(Individuo(np.random.rand(self.numeroNeuronas*(self.numeroEntradas+1)+2)))
+            #Generacion de indiviuos simple, todo de 0 a 1
+            #self.poblacion.append(Individuo(list(np.random.rand(self.numeroNeuronas*(self.numeroEntradas+1)+2))))
+            #Generacion de individuos: la primera capa de -1 a 1, la segunda capa de -10 a 10 y el bias de 0 a 1 
+            capaOculta = [(random.random()-0.5)*2 for x in range(self.numeroNeuronas*self.numeroEntradas)]
+            capaSalida = [(random.random()-0.5)*14 for x in range(self.numeroNeuronas)]
+            bias = [random.random(),random.random()]
+            cromosoma = capaOculta+capaSalida+bias
+            self.poblacion.append(Individuo(cromosoma))
+        generacion = 0
+        mejorError = 9999999
         #evaluar la poblacionInicial
-        sumatoriaAptitud = 0
-        for ind in self.poblacion:
-            pesosCapaOculta = np.array([ind.cromosoma[(x*self.numeroEntradas):((x*self.numeroEntradas)+self.numeroEntradas)] for x in range(self.numeroNeuronas)])
-            pesosCapaSalida = np.array(ind.cromosoma[(55*self.numeroNeuronas):((self.numeroEntradas*self.numeroNeuronas)+self.numeroNeuronas)])
-            biasCapaOculta = ind.cromosoma[-2]
-            biasCapaSalida = ind.cromosoma[-1]
-            ind.evaluacion = self.redNeuronal.ProbarModelo(pesosCapaOculta, pesosCapaSalida, biasCapaOculta, biasCapaSalida)
-            ind.aptitud = (1/(ind.evaluacion))*100
-            sumatoriaAptitud += ind.aptitud
-        #fin evaluacion
-        seleccionados = []# arreglo con los individuos que seran seleccionados
-        #for para seleccionar los individuos
-        for x in range(int(self.tamanoPoblacion/2)):
-            ruleta = random.random()*sumatoriaAptitud 
-            sumaBusqueda = 0
+        while True:
+            #print("generacion: {}".format(generacion))
+            sumatoriaAptitud = 0
             for ind in self.poblacion:
-                sumaBusqueda += ind.aptitud
-                if ruleta < sumaBusqueda:
-                    seleccionados.append(ind)
-                    break
-        #fin de seleccionar los individuos
-        #cruce de individuos
-        hijos = []
-        for x in range(int(len(seleccionados)/2)):
-            padre1 = seleccionados[(2*x)]
-            padre2 = seleccionados[(2*x)+1]
-            hijo1=[]
-            hijo2=[]
-            for x in range(len(padre1.cromosoma)):
-                print(np.random.standard_normal())
-                if random.random()<0.5:
-                    hijo1.append(padre1.cromosoma[x])
-                    hijo2.append(padre2.cromosoma[x])
-                else:
-                    hijo2.append(padre1.cromosoma[x])
-                    hijo1.append(padre2.cromosoma[x])
-            hijos.append(Individuo(hijo1))
-            hijos.append(Individuo(hijo2))
-        # fin del cruce y creacion de los hijos
-        self.poblacion.sort(key=lambda ind: ind.aptitud, reverse=True)# ordenar la poblacion para poder hacer el reemplazo
-        # reemplazo
-        self.poblacion =  self.poblacion[:int(self.tamanoPoblacion/2)]+hijos# se elimina una mitad y se le agrega el arreglo de hijos
-
+                pesosCapaOculta = np.array([ind.cromosoma[(x*self.numeroEntradas):((x*self.numeroEntradas)+self.numeroEntradas)] for x in range(self.numeroNeuronas)])
+                pesosCapaSalida = np.array(ind.cromosoma[(55*self.numeroNeuronas):((self.numeroEntradas*self.numeroNeuronas)+self.numeroNeuronas)])
+                biasCapaOculta = ind.cromosoma[-2]
+                biasCapaSalida = ind.cromosoma[-1]
+                ind.evaluacion = self.redNeuronal.ProbarModelo(pesosCapaOculta, pesosCapaSalida, biasCapaOculta, biasCapaSalida)
+                ind.aptitud = (1/(ind.evaluacion))*100
+                ind.precision = self.redNeuronal.precision
+                # donde se guarda aparte cada modelo que va siendo mejor que el anterior
+                if(ind.evaluacion<mejorError):
+                    mejorError = ind.evaluacion
+                    F = open("../resultadosEvolutivos/mejorResultado"+str(generacion)+".txt","w")
+                    F.write("\n pesos capa oculta 1\n")
+                    for m in range(len(pesosCapaOculta)):
+                            F.write(str(pesosCapaOculta[m]))
+                    F.write("\n pesos capa salida\n")
+                    F.write(str(pesosCapaSalida))
+                    F.write("\n biasCapaoculta\n")
+                    F.write(str(biasCapaOculta))
+                    F.write("\n biasCapaoSalida\n")
+                    F.write(str(biasCapaSalida))
+                    F.write("\n mejor error\n")
+                    F.write(str(mejorError))
+                    F.write("\n Precision \n")
+                    F.write(str(ind.precision))
+                    F.close()
+                    print(ind.precision)
+                # fin if
+                sumatoriaAptitud += ind.aptitud
+            #fin evaluacion
+            #for donde se guarda toda la generacion
+            ''''F = open("../generaciones/generacion"+str(generacion)+".txt","w")
+            F.write("Generacion numero"+str(generacion)+"\n")
+            for ind in self.poblacion:
+                    F.write("Precision:  ")
+                    F.write(str(ind.precision))
+                    F.write("   Error: ")
+                    F.write(str(ind.evaluacion))
+                    F.write("   Aptitud: ")
+                    F.write(str(ind.aptitud))
+                    F.write("   Cromosoma:  ")
+                    F.write(str(ind.cromosoma))
+                    F.write("\n")
+            F.close()'''
+            #fin for 
+            seleccionados = []# arreglo con los individuos que seran seleccionados
+            #for para seleccionar los individuos
+            for x in range(int(self.tamanoPoblacion/2)):
+                ruleta = random.random()*sumatoriaAptitud 
+                sumaBusqueda = 0
+                for ind in self.poblacion:
+                    sumaBusqueda += ind.aptitud
+                    if ruleta < sumaBusqueda:
+                        seleccionados.append(ind)
+                        break
+            #fin de seleccionar los individuos
+            #cruce de individuos
+            hijos = []
+            for x in range(int(len(seleccionados)/2)):
+                padre1 = seleccionados[(2*x)]
+                padre2 = seleccionados[(2*x)+1]
+                hijo1=[]
+                hijo2=[]
+                for x in range(len(padre1.cromosoma)):
+                    if random.random()<0.5:# la probabilidad igual para hacer el cruce o dejar igual
+                        hijo1.append(padre1.cromosoma[x])
+                        hijo2.append(padre2.cromosoma[x])
+                    else:
+                        hijo2.append(padre1.cromosoma[x])
+                        hijo1.append(padre2.cromosoma[x])
+                        
+                    if random.random()<0.08:# probabilidad de mutacion para el hijo1 en el gen x
+                        antes = hijo1[x]
+                        hijo1[x] += (np.random.standard_normal()*4)
+                        
+                    if random.random()<0.08:# probabilidad de mutacion para el hijo2 en el gen x
+                        hijo2[x] += (np.random.standard_normal()*4)
+    
+                hijos.append(Individuo(hijo1))
+                hijos.append(Individuo(hijo2))
+            # fin del cruce y creacion de los hijos
+            self.poblacion.sort(key=lambda ind: ind.aptitud, reverse=True)# ordenar la poblacion para poder hacer el reemplazo
+            # reemplazo
+            self.poblacion =  self.poblacion[:int(self.tamanoPoblacion/2)]+hijos# se elimina una mitad y se le agrega el arreglo de hijos
+            generacion += 1
+        #fin while
         
 
         
 
             
   
-e = Algoritmoevolutivo(24,3,55)
+e = Algoritmoevolutivo(100,19,55)
 e.evolucion()
